c356e8ee14cfa7d1a12e4283d9dafc5f5b47f78e
carloskael32 <carloskael32@gmail.com>
2026-01-25 13:50:44 -0400

    datatable primevue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,25 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+//datatable primevue
+import Aura from '@primevue/themes/aura'
+
+import PrimeVue from 'primevue/config';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Toolbar from 'primevue/toolbar';
+import InputText from 'primevue/inputtext';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import Button from 'primevue/button';
+
+
+import 'primeicons/primeicons.css'; // Icons
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -17,12 +34,28 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue'),
         ),
     setup({ el, App, props, plugin }) {
+
+
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
+            .component('Datatable', DataTable)
+            .component('Column', Column)
+            .component('Toolbar', Toolbar)
+            .component('InputText', InputText)
+            .component('IconField', IconField)
+            .component('InputIcon', InputIcon)
+            .component('Button', Button)
             .mount(el);
+
     },
     progress: {
         color: '#4B5563',
     },
+
 });
